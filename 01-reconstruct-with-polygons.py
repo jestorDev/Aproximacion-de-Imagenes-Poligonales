6ba3ac284b77dbb02920a161a9ba00556fa57386
Jesus <jtorrejon@ieduca.pe>
2022-07-17 16:20:24 -0500

    Implementacion de metodo del torneo

--- 01-reconstruct-with-polygons.py
+++ 01-reconstruct-with-polygons.py
@@ -294,6 +294,30 @@ def getDiff(individual):
 toolbox.register("evaluate", getDiff)
 
 ##########################################################################################
+##########################################################################################
+# Metodo de seleccion de participantes
+##########################################################################################
+
+
+def selccionTorneo(individuals, k, tournsize, fit_attr="fitness"):
+    """Select the best individual among *tournsize* randomly chosen
+    individuals, *k* times. The list returned contains
+    references to the input *individuals*.
+
+    :param individuals: A list of individuals to select from.
+    :param k: The number of individuals to select.
+    :param tournsize: The number of individuals participating in each tournament.
+    :param fit_attr: The attribute of individuals to use as selection criterion
+    :returns: A list of selected individuals.
+
+    This function uses the :func:`~random.choice` function from the python base
+    :mod:`random` module.
+    """
+    chosen = []
+    for i in range(k):
+        aspirants = [random.choice(individuals) for i in range(tournsize)]
+        chosen.append(max(aspirants, key=attrgetter(fit_attr)))
+    return chosen
 
 
 # genetic operators:
