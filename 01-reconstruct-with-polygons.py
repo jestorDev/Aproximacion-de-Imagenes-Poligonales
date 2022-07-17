e507883bcb267ef5fa26571152a1f3493c880425
Jesus <jtorrejon@ieduca.pe>
2022-07-17 16:19:57 -0500

    Defined class Image test for image processing

--- 01-reconstruct-with-polygons.py
+++ 01-reconstruct-with-polygons.py
@@ -3,14 +3,18 @@ from deap import creator
 from deap import tools
 
 import random
-import numpy
+import numpy  as np
 import os
 
 import image_test
-import elitism_callback
+import cv2
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+from deap import algorithms
+
+from operator import attrgetter
+
 
 # problem related constants
 POLYGON_SIZE = 3
@@ -26,8 +30,8 @@ POPULATION_SIZE = 200
 P_CROSSOVER = 0.9  # probability for crossover
 P_MUTATION = 0.5   # probability for mutating an individual
 MAX_GENERATIONS = 100
-HALL_OF_FAME_SIZE = 20
-CROWDING_FACTOR = 10.0  # crowding factor for crossover and mutation
+#HALL_OF_FAME_SIZE = 20
+#CROWDING_FACTOR = 10.0  # crowding factor for crossover and mutation
 
 # set the random seed:
 RANDOM_SEED = 42
@@ -72,6 +76,215 @@ toolbox.register("populationCreator",
                  list,
                  toolbox.individualCreator)
 
+##########################################################################################
+    #Fitnesss
+
+
+class ImageTest:
+
+    def __init__(self, imagePath, polygonSize):
+        """
+        Initializes an instance of the class
+        :param imagePath: the path of the file containing the reference image
+        :param polygonSize: the number of vertices on the polygons used to recreate the image
+        """
+        self.refImage = Image.open(imagePath)
+        self.polygonSize = polygonSize
+
+        self.width, self.height = self.refImage.size
+        self.numPixels = self.width * self.height
+        self.refImageCv2 = self.toCv2(self.refImage)
+
+
+    def polygonDataToImagea(self, polygonData):
+        
+        """
+        accepts polygon data and creates an image containing these polygons.
+        :param polygonData: a list of polygon parameters. Each item in the list
+        represents the vertices locations, color and transparency of the corresponding polygon
+        :return: the image containing the polygons (Pillow format)
+        """
+        #print (len(polygonData))
+
+        #print ("---------------Polygon data------------------------------")
+        #print (polygonData)
+
+
+
+        
+        # start with a new image:
+
+        ima = np.ones((self.width,self.height,3), np.int8)
+        #image = Image.new('RGB', (self.width, self.height))#TODO
+        #draw = ImageDraw.Draw(image, 'RGBA')
+
+        # divide the polygonData to chunks, each containing the data for a single polygon:
+        chunkSize = self.polygonSize * 2 + 4  # (x,y) per vertex + (RGBA)
+        
+        arrR = np.array(polygonData).reshape(-1 , chunkSize)
+        arrVertex = arrR[ :,:self.polygonSize*2 ].reshape( (-1 ,self.polygonSize,2)) 
+        arrColor = arrR[ :,self.polygonSize*2:-1 ]
+
+        arrVertex[ : , : , 1]  *= self.height
+        arrVertex[ : , : , 0]  *= self.width 
+        arrColor *= 255
+
+        for vertexArr , arrColor in zip(arrVertex , arrColor):
+            #print(vertexArr , arrColor)
+            
+            cv2.drawContours(ima,  np.int32([vertexArr]), 0, tuple(arrColor), -1)
+            #                                                      r   g   b
+            # cv2.fillPoly(ima, pts = np.int32([vertexArr]) , color=(0, 0, 0 ))
+            #draw.polygon(vertices, (red, green, blue, alpha))
+
+        # cleanup:
+        #del draw
+        # https://gist.github.com/IAmSuyogJadhav/305bfd9a0605a4c096383408bee7fd5c
+
+
+        return   cv2.cvtColor(ima, cv2.COLOR_RGB2BGR)
+
+    def polygonDataToImage(self, polygonData):
+        """
+        accepts polygon data and creates an image containing these polygons.
+        :param polygonData: a list of polygon parameters. Each item in the list
+        represents the vertices locations, color and transparency of the corresponding polygon
+        :return: the image containing the polygons (Pillow format)
+        """
+
+        # start with a new image:
+        image = Image.new('RGB', (self.width, self.height))#TODO
+        draw = ImageDraw.Draw(image, 'RGBA')
+
+        # divide the polygonData to chunks, each containing the data for a single polygon:
+        chunkSize = self.polygonSize * 2 + 4  # (x,y) per vertex + (RGBA)
+        polygons = self.list2Chunks(polygonData, chunkSize)
+
+        # iterate over all polygons and draw each of them into the image:
+        for poly in polygons:
+            index = 0
+
+            # extract the vertices of the current polygon:
+            vertices = []
+            for vertex in range(self.polygonSize):
+                vertices.append((int(poly[index] * self.width), int(poly[index + 1] * self.height)))
+                index += 2
+
+            # extract the RGB and alpha values of the current polygon:
+            red = int(poly[index] * 255)
+            green = int(poly[index + 1] * 255)
+            blue = int(poly[index + 2] * 255)
+            alpha = int(poly[index + 3] * 255)
+
+            # draw the polygon into the image:
+            draw.polygon(vertices, (red, green, blue, alpha))
+
+        # cleanup:
+        del draw
+
+        return image
+
+    def getDifference(self, polygonData, method="MSE"):
+        """
+        accepts polygon data, creates an image containing these polygons, and calculates the difference
+        between this image and the reference image using one of two methods.
+        :param polygonData: a list of polygon parameters. Each item in the list
+        represents the vertices locations, color and transparency of the corresponding polygon
+        :param method: base method of calculating the difference ("MSE" or "SSIM").
+        larger return value always means larger difference
+        :return: the calculated difference between the image containg the polygons and the reference image
+        """
+
+        # create the image containing the polygons:
+        image = self.polygonDataToImage(polygonData)
+
+        if method == "MSE":
+            return self.getMse(image)
+        else:
+            return 1.0 - self.getSsim(image)
+
+    def plotImages(self, image, header=None):
+        """
+        creates a 'side-by-side' plot of the given image next to the reference image
+        :param image: image to be drawn next to reference image (Pillow format)
+        :param header: text used as a header for the plot
+        """
+
+        fig = plt.figure("Image Comparison:")
+        if header:
+            plt.suptitle(header)
+
+        # plot the reference image on the left:
+        ax = fig.add_subplot(1, 2, 1)
+        plt.imshow(self.refImage)
+        self.ticksOff(plt)
+
+        # plot the given image on the right:
+        fig.add_subplot(1, 2, 2)
+        plt.imshow(image)
+        self.ticksOff(plt)
+
+        return plt
+
+    def saveImage(self, polygonData, imageFilePath, header=None):
+        """
+        accepts polygon data, creates an image containing these polygons,
+        creates a 'side-by-side' plot of this image next to the reference image,
+        and saves the plot to a file
+        :param polygonData: a list of polygon parameters. Each item in the list
+        represents the vertices locations, color and transparency of the corresponding polygon
+        :param imageFilePath: path of file to be used to save the plot to
+        :param header: text used as a header for the plot
+        """
+
+        # create an image from th epolygon data:
+        image = self.polygonDataToImage(polygonData)
+
+        # plot the image side-by-side with the reference image:
+        self.plotImages(image, header)
+
+        # save the plot to file:
+        plt.savefig(imageFilePath)
+
+    # utility methods:
+
+    def toCv2(self, pil_image):
+        """converts the given Pillow image to CV2 format"""
+        return cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+
+    def getMse(self, image):
+        """calculates MSE of difference between the given image and the reference image"""
+        return np.sum((self.toCv2(image).astype("float") - self.refImageCv2.astype("float")) ** 2)/float(self.numPixels)
+
+    def getMseCV(self, image):
+        """calculates MSE of difference between the given image and the reference image"""
+        return np.sum((image.astype("float") - self.refImageCv2.astype("float")) ** 2)/float(self.numPixels)
+
+
+    def getSsim(self, image):
+        """calculates mean structural similarity index between the given image and the reference image"""
+        return structural_similarity(self.toCv2(image), self.refImageCv2, multichannel=True)
+
+    def list2Chunks(self, list, chunkSize):
+        """divides a given list to fixed size chunks, returns a generator iterator"""
+        for chunk in range(0, len(list), chunkSize):
+            yield(list[chunk:chunk + chunkSize])
+
+    def ticksOff(self, plot):#TODO
+        """turns off ticks on both axes"""
+        plt.tick_params(
+            axis='both',
+            which='both',
+            bottom=False,
+            left=False,
+            top=False,
+            right=False,
+            labelbottom=False,
+            labelleft=False,
+        )
+
+
+
 
 # fitness calculation using MSE as difference metric:
 def getDiff(individual):
@@ -80,21 +293,43 @@ def getDiff(individual):
 
 toolbox.register("evaluate", getDiff)
 
+##########################################################################################
+
 
 # genetic operators:
-toolbox.register("select", tools.selTournament, tournsize=2)
+toolbox.register("select", selccionTorneo , tournsize=2)
 
 toolbox.register("mate",
-                 tools.cxSimulatedBinaryBounded,
-                 low=BOUNDS_LOW,
-                 up=BOUNDS_HIGH,
-                 eta=CROWDING_FACTOR)
+                 tools.cxTwoPoint)
+
+
+
+
+def mutUniformFloat(individual, low, up, indpb):
+    """Mutate an individual by replacing attributes, with probability *indpb*,
+    by a integer uniformly drawn between *low* and *up* inclusively.
+
+    :param individual: :term:`Sequence <sequence>` individual to be mutated.
+    :param low: The lower bound or a :term:`python:sequence` of
+                of lower bounds of the range from wich to draw the new
+                integer.
+    :param up: The upper bound or a :term:`python:sequence` of
+               of upper bounds of the range from wich to draw the new
+               integer.
+    :param indpb: Independent probability for each attribute to be mutated.
+    :returns: A tuple of one individual.
+    """
+
+    for i in range(len(individual)):
+        if random.random() < indpb:
+            individual[i] = random.uniform(low, up)
+
+    return individual,
 
 toolbox.register("mutate",
-                 tools.mutPolynomialBounded,
+                 mutUniformFloat,
                  low=BOUNDS_LOW,
                  up=BOUNDS_HIGH,
-                 eta=CROWDING_FACTOR,
                  indpb=1.0/NUM_OF_PARAMS)
 
 
@@ -122,14 +357,15 @@ def main():
 
     # prepare the statistics object:
     stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("min", numpy.min)
-    stats.register("avg", numpy.mean)
+    stats.register("min", np.min)
+    stats.register("avg", np.mean)
 
     # define the hall-of-fame object:
-    hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
+    #hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
 
 
     # perform the Genetic Algorithm flow with elitism and 'saveImage' callback:
+    """
     population, logbook = elitism_callback.eaSimpleWithElitismAndCallback(population,
                                                       toolbox,
                                                       cxpb=P_CROSSOVER,
@@ -139,20 +375,28 @@ def main():
                                                       stats=stats,
                                                       halloffame=hof,
                                                       verbose=True)
+    """
+    population, logbook = algorithms.eaSimple(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION, ngen=MAX_GENERATIONS,
+                                   stats=stats, verbose=True)
+
 
     # print best solution found:
-    best = hof.items[0]
+    best = population[0]
     print()
     print("Best Solution = ", best)
     print("Best Score = ", best.fitness.values[0])
     print()
-
     # draw best image next to reference image:
     imageTest.plotImages(imageTest.polygonDataToImage(best))
 
     # extract statistics:
     minFitnessValues, meanFitnessValues = logbook.select("min", "avg")
 
+    print("Fitness minimo : ")
+    print(minFitnessValues)
+    
+    print("Fitness promedio : ")
+    print(meanFitnessValues)
     # plot statistics:
     sns.set_style("whitegrid")
     plt.figure("Stats:")
---
+++ test_srcipting.py
@@ -0,0 +1,6 @@
+
+import sys
+import time
+time.sleep(3)
+list1 = sys.argv[1].split(',')
+print (list1)
