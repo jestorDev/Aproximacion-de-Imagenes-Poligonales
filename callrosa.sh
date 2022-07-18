9c0dbb4ecc776ed7a2d8a4b93e53ff8cdd69e624
Jesus <jtorrejon@ieduca.pe>
2022-07-17 22:53:24 -0500

    cambiado a linux

---
+++ callrosa.sh
@@ -0,0 +1,315 @@
+python aproximacion_poligonal.py 200,0.9,0.6,1
+python aproximacion_poligonal.py 200,0.9,0.6,2
+python aproximacion_poligonal.py 200,0.9,0.6,3
+python aproximacion_poligonal.py 200,0.9,0.6,4
+python aproximacion_poligonal.py 200,0.9,0.6,5
+
+python aproximacion_poligonal.py  50,0.8,0.7,1
+python aproximacion_poligonal.py  50,0.8,0.7,2
+python aproximacion_poligonal.py  50,0.8,0.7,3
+python aproximacion_poligonal.py  50,0.8,0.7,4
+python aproximacion_poligonal.py  50,0.8,0.7,5
+
+python aproximacion_poligonal.py  150,0.7,0.6,1
+python aproximacion_poligonal.py  150,0.7,0.6,2
+python aproximacion_poligonal.py  150,0.7,0.6,3
+python aproximacion_poligonal.py  150,0.7,0.6,4
+python aproximacion_poligonal.py  150,0.7,0.6,5
+
+python aproximacion_poligonal.py  150,0.8,0.6,1
+python aproximacion_poligonal.py  150,0.8,0.6,2
+python aproximacion_poligonal.py  150,0.8,0.6,3
+python aproximacion_poligonal.py  150,0.8,0.6,4
+python aproximacion_poligonal.py  150,0.8,0.6,5
+
+python aproximacion_poligonal.py  200,0.6,0.5,1
+python aproximacion_poligonal.py  200,0.6,0.5,2
+python aproximacion_poligonal.py  200,0.6,0.5,3
+python aproximacion_poligonal.py  200,0.6,0.5,4
+python aproximacion_poligonal.py  200,0.6,0.5,5
+
+python aproximacion_poligonal.py  80,0.8,0.6,1
+python aproximacion_poligonal.py  80,0.8,0.6,2
+python aproximacion_poligonal.py  80,0.8,0.6,3
+python aproximacion_poligonal.py  80,0.8,0.6,4
+python aproximacion_poligonal.py  80,0.8,0.6,5
+
+python aproximacion_poligonal.py  50,0.6,0.3,1
+python aproximacion_poligonal.py  50,0.6,0.3,2
+python aproximacion_poligonal.py  50,0.6,0.3,3
+python aproximacion_poligonal.py  50,0.6,0.3,4
+python aproximacion_poligonal.py  50,0.6,0.3,5
+
+python aproximacion_poligonal.py  150,0.9,0.4,1
+python aproximacion_poligonal.py  150,0.9,0.4,2
+python aproximacion_poligonal.py  150,0.9,0.4,3
+python aproximacion_poligonal.py  150,0.9,0.4,4
+python aproximacion_poligonal.py  150,0.9,0.4,5
+
+python aproximacion_poligonal.py  100,0.9,0.2,1
+python aproximacion_poligonal.py  100,0.9,0.2,2
+python aproximacion_poligonal.py  100,0.9,0.2,3
+python aproximacion_poligonal.py  100,0.9,0.2,4
+python aproximacion_poligonal.py  100,0.9,0.2,5
+
+python aproximacion_poligonal.py  50,0.9,0.7,1
+python aproximacion_poligonal.py  50,0.9,0.7,2
+python aproximacion_poligonal.py  50,0.9,0.7,3
+python aproximacion_poligonal.py  50,0.9,0.7,4
+python aproximacion_poligonal.py  50,0.9,0.7,5
+
+python aproximacion_poligonal.py  50,0.7,0.1,1
+python aproximacion_poligonal.py  50,0.7,0.1,2
+python aproximacion_poligonal.py  50,0.7,0.1,3
+python aproximacion_poligonal.py  50,0.7,0.1,4
+python aproximacion_poligonal.py  50,0.7,0.1,5
+
+python aproximacion_poligonal.py  80,0.6,0.7,1
+python aproximacion_poligonal.py  80,0.6,0.7,2
+python aproximacion_poligonal.py  80,0.6,0.7,3
+python aproximacion_poligonal.py  80,0.6,0.7,4
+python aproximacion_poligonal.py  80,0.6,0.7,5
+
+python aproximacion_poligonal.py  80,0.9,0.3,1
+python aproximacion_poligonal.py  80,0.9,0.3,2
+python aproximacion_poligonal.py  80,0.9,0.3,3
+python aproximacion_poligonal.py  80,0.9,0.3,4
+python aproximacion_poligonal.py  80,0.9,0.3,5
+
+python aproximacion_poligonal.py  150,0.6,0.5,1
+python aproximacion_poligonal.py  150,0.6,0.5,2
+python aproximacion_poligonal.py  150,0.6,0.5,3
+python aproximacion_poligonal.py  150,0.6,0.5,4
+python aproximacion_poligonal.py  150,0.6,0.5,5
+
+python aproximacion_poligonal.py  50,0.8,0.2,1
+python aproximacion_poligonal.py  50,0.8,0.2,2
+python aproximacion_poligonal.py  50,0.8,0.2,3
+python aproximacion_poligonal.py  50,0.8,0.2,4
+python aproximacion_poligonal.py  50,0.8,0.2,5
+
+python aproximacion_poligonal.py  100,0.8,0.6,1
+python aproximacion_poligonal.py  100,0.8,0.6,2
+python aproximacion_poligonal.py  100,0.8,0.6,3
+python aproximacion_poligonal.py  100,0.8,0.6,4
+python aproximacion_poligonal.py  100,0.8,0.6,5
+
+python aproximacion_poligonal.py  200,0.6,0.2,1
+python aproximacion_poligonal.py  200,0.6,0.2,2
+python aproximacion_poligonal.py  200,0.6,0.2,3
+python aproximacion_poligonal.py  200,0.6,0.2,4
+python aproximacion_poligonal.py  200,0.6,0.2,5
+
+python aproximacion_poligonal.py  50,0.7,0.3,1
+python aproximacion_poligonal.py  50,0.7,0.3,2
+python aproximacion_poligonal.py  50,0.7,0.3,3
+python aproximacion_poligonal.py  50,0.7,0.3,4
+python aproximacion_poligonal.py  50,0.7,0.3,5
+
+python aproximacion_poligonal.py  80,0.9,0.5,1
+python aproximacion_poligonal.py  80,0.9,0.5,2
+python aproximacion_poligonal.py  80,0.9,0.5,3
+python aproximacion_poligonal.py  80,0.9,0.5,4
+python aproximacion_poligonal.py  80,0.9,0.5,5
+
+python aproximacion_poligonal.py  80,0.8,0.7,1
+python aproximacion_poligonal.py  80,0.8,0.7,2
+python aproximacion_poligonal.py  80,0.8,0.7,3
+python aproximacion_poligonal.py  80,0.8,0.7,4
+python aproximacion_poligonal.py  80,0.8,0.7,5
+
+python aproximacion_poligonal.py  80,0.8,0.5,1
+python aproximacion_poligonal.py  80,0.8,0.5,2
+python aproximacion_poligonal.py  80,0.8,0.5,3
+python aproximacion_poligonal.py  80,0.8,0.5,4
+python aproximacion_poligonal.py  80,0.8,0.5,5
+
+python aproximacion_poligonal.py  100,0.8,0.3,1
+python aproximacion_poligonal.py  100,0.8,0.3,2
+python aproximacion_poligonal.py  100,0.8,0.3,3
+python aproximacion_poligonal.py  100,0.8,0.3,4
+python aproximacion_poligonal.py  100,0.8,0.3,5
+
+python aproximacion_poligonal.py  50,0.6,0.2,1
+python aproximacion_poligonal.py  50,0.6,0.2,2
+python aproximacion_poligonal.py  50,0.6,0.2,3
+python aproximacion_poligonal.py  50,0.6,0.2,4
+python aproximacion_poligonal.py  50,0.6,0.2,5
+
+python aproximacion_poligonal.py  50,0.9,0.3,1
+python aproximacion_poligonal.py  50,0.9,0.3,2
+python aproximacion_poligonal.py  50,0.9,0.3,3
+python aproximacion_poligonal.py  50,0.9,0.3,4
+python aproximacion_poligonal.py  50,0.9,0.3,5
+
+python aproximacion_poligonal.py  150,0.6,0.3,1
+python aproximacion_poligonal.py  150,0.6,0.3,2
+python aproximacion_poligonal.py  150,0.6,0.3,3
+python aproximacion_poligonal.py  150,0.6,0.3,4
+python aproximacion_poligonal.py  150,0.6,0.3,5
+
+python aproximacion_poligonal.py  200,0.6,0.6,1
+python aproximacion_poligonal.py  200,0.6,0.6,2
+python aproximacion_poligonal.py  200,0.6,0.6,3
+python aproximacion_poligonal.py  200,0.6,0.6,4
+python aproximacion_poligonal.py  200,0.6,0.6,5
+
+python aproximacion_poligonal.py  100,0.7,0.1,1
+python aproximacion_poligonal.py  100,0.7,0.1,2
+python aproximacion_poligonal.py  100,0.7,0.1,3
+python aproximacion_poligonal.py  100,0.7,0.1,4
+python aproximacion_poligonal.py  100,0.7,0.1,5
+
+python aproximacion_poligonal.py  100,0.6,0.4,1
+python aproximacion_poligonal.py  100,0.6,0.4,2
+python aproximacion_poligonal.py  100,0.6,0.4,3
+python aproximacion_poligonal.py  100,0.6,0.4,4
+python aproximacion_poligonal.py  100,0.6,0.4,5
+
+python aproximacion_poligonal.py  80,0.7,0.3,1
+python aproximacion_poligonal.py  80,0.7,0.3,2
+python aproximacion_poligonal.py  80,0.7,0.3,3
+python aproximacion_poligonal.py  80,0.7,0.3,4
+python aproximacion_poligonal.py  80,0.7,0.3,5
+
+python aproximacion_poligonal.py  150,0.8,0.5,1
+python aproximacion_poligonal.py  150,0.8,0.5,2
+python aproximacion_poligonal.py  150,0.8,0.5,3
+python aproximacion_poligonal.py  150,0.8,0.5,4
+python aproximacion_poligonal.py  150,0.8,0.5,5
+
+python aproximacion_poligonal.py  50,0.8,0.3,1
+python aproximacion_poligonal.py  50,0.8,0.3,2
+python aproximacion_poligonal.py  50,0.8,0.3,3
+python aproximacion_poligonal.py  50,0.8,0.3,4
+python aproximacion_poligonal.py  50,0.8,0.3,5
+
+python aproximacion_poligonal.py  80,0.9,0.1,1
+python aproximacion_poligonal.py  80,0.9,0.1,2
+python aproximacion_poligonal.py  80,0.9,0.1,3
+python aproximacion_poligonal.py  80,0.9,0.1,4
+python aproximacion_poligonal.py  80,0.9,0.1,5
+
+python aproximacion_poligonal.py  100,0.9,0.5,1
+python aproximacion_poligonal.py  100,0.9,0.5,2
+python aproximacion_poligonal.py  100,0.9,0.5,3
+python aproximacion_poligonal.py  100,0.9,0.5,4
+python aproximacion_poligonal.py  100,0.9,0.5,5
+
+python aproximacion_poligonal.py  200,0.8,0.7,1
+python aproximacion_poligonal.py  200,0.8,0.7,2
+python aproximacion_poligonal.py  200,0.8,0.7,3
+python aproximacion_poligonal.py  200,0.8,0.7,4
+python aproximacion_poligonal.py  200,0.8,0.7,5
+
+python aproximacion_poligonal.py  200,0.7,0.6,1
+python aproximacion_poligonal.py  200,0.7,0.6,2
+python aproximacion_poligonal.py  200,0.7,0.6,3
+python aproximacion_poligonal.py  200,0.7,0.6,4
+python aproximacion_poligonal.py  200,0.7,0.6,5
+
+python aproximacion_poligonal.py  200,0.6,0.4,1
+python aproximacion_poligonal.py  200,0.6,0.4,2
+python aproximacion_poligonal.py  200,0.6,0.4,3
+python aproximacion_poligonal.py  200,0.6,0.4,4
+python aproximacion_poligonal.py  200,0.6,0.4,5
+
+python aproximacion_poligonal.py  150,0.7,0.1,1
+python aproximacion_poligonal.py  150,0.7,0.1,2
+python aproximacion_poligonal.py  150,0.7,0.1,3
+python aproximacion_poligonal.py  150,0.7,0.1,4
+python aproximacion_poligonal.py  150,0.7,0.1,5
+
+python aproximacion_poligonal.py  200,0.7,0.3,1
+python aproximacion_poligonal.py  200,0.7,0.3,2
+python aproximacion_poligonal.py  200,0.7,0.3,3
+python aproximacion_poligonal.py  200,0.7,0.3,4
+python aproximacion_poligonal.py  200,0.7,0.3,5
+
+python aproximacion_poligonal.py  200,0.9,0.7,1
+python aproximacion_poligonal.py  200,0.9,0.7,2
+python aproximacion_poligonal.py  200,0.9,0.7,3
+python aproximacion_poligonal.py  200,0.9,0.7,4
+python aproximacion_poligonal.py  200,0.9,0.7,5
+
+python aproximacion_poligonal.py  100,0.9,0.3,1
+python aproximacion_poligonal.py  100,0.9,0.3,2
+python aproximacion_poligonal.py  100,0.9,0.3,3
+python aproximacion_poligonal.py  100,0.9,0.3,4
+python aproximacion_poligonal.py  100,0.9,0.3,5
+
+python aproximacion_poligonal.py  200,0.6,0.7,1
+python aproximacion_poligonal.py  200,0.6,0.7,2
+python aproximacion_poligonal.py  200,0.6,0.7,3
+python aproximacion_poligonal.py  200,0.6,0.7,4
+python aproximacion_poligonal.py  200,0.6,0.7,5
+
+python aproximacion_poligonal.py  50,0.9,0.6,1
+python aproximacion_poligonal.py  50,0.9,0.6,2
+python aproximacion_poligonal.py  50,0.9,0.6,3
+python aproximacion_poligonal.py  50,0.9,0.6,4
+python aproximacion_poligonal.py  50,0.9,0.6,5
+
+python aproximacion_poligonal.py  80,0.9,0.7,1
+python aproximacion_poligonal.py  80,0.9,0.7,2
+python aproximacion_poligonal.py  80,0.9,0.7,3
+python aproximacion_poligonal.py  80,0.9,0.7,4
+python aproximacion_poligonal.py  80,0.9,0.7,5
+
+python aproximacion_poligonal.py  80,0.8,0.4,1
+python aproximacion_poligonal.py  80,0.8,0.4,2
+python aproximacion_poligonal.py  80,0.8,0.4,3
+python aproximacion_poligonal.py  80,0.8,0.4,4
+python aproximacion_poligonal.py  80,0.8,0.4,5
+
+python aproximacion_poligonal.py  50,0.6,0.6,1
+python aproximacion_poligonal.py  50,0.6,0.6,2
+python aproximacion_poligonal.py  50,0.6,0.6,3
+python aproximacion_poligonal.py  50,0.6,0.6,4
+python aproximacion_poligonal.py  50,0.6,0.6,5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
